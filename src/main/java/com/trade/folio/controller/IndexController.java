a0793d25ef07e1a22587e63f341517c147564c7d
Je <michelanjeho@gmail.com>
2026-02-15 15:14:04 +0900

    Revert "join"
    This reverts commit a3d79fc52ff751d5ab99cfa619967bd07d0bb35c.

--- src/main/java/com/trade/folio/controller/IndexController.java
+++ src/main/java/com/trade/folio/controller/IndexController.java
@@ -1,4 +1,4 @@
-package com.trade.folio.common.controller;
+package com.trade.folio.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
